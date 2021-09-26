19bd2bbaf8d9bea19abb9cf595529b1a8c4c88f3
chicha099 <tdsserafin@gmail.com>
2021-09-25 21:19:21 -0300

    models en progreso

--- api/src/models/Tipos.js
+++ api/src/models/Tipos.js
@@ -4,6 +4,7 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
     // defino el modelo
     sequelize.define('tipos', {
-
+        name: type.STRING,
+        allowNull: false,
     });
 };
