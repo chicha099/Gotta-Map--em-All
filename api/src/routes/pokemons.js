0a7d8e61c879331ba4db9958efb7fee651cf07bb
chicha099 <tdsserafin@gmail.com>
2021-09-26 21:35:08 -0300

    Trying to get all pokemons info

--- api/src/routes/pokemons.js
+++ api/src/routes/pokemons.js
@@ -1,18 +1,34 @@
 const { Router } = require("express");
-const { fetch } = require("node-fetch");
+const axios = require('axios');
 const { Pokemon } = require("../db.js");
 
 const router = Router();
 
-// router.get('/', async (req, res) => {
-//     try {
-//         const api = await fetch('https://pokeapi.co/api/v2/pokemon');
-//         const apiJson = api.json();
-//         const boldPokeArray = apiJson.results;
-//         return res.json(boldPokeArray);
-//     } catch (error) {
-//         console.log(error)
-//     }
-// })
+router.get("/", (req, res, next) => {
+    axios.get('https://pokeapi.co/api/v2/pokemon')
+        .then(resp => {
+            const next = resp.data.next;
+            const firstGet = resp.data.results;
+            axios.get(next)
+                .then(nextResp => {
+                    const secondGet = nextResp.data.results;
+                    const allPokemonsRaw = firstGet.concat(secondGet);
+                    for (let i = 0; i < allPokemonsRaw.length; i++) {
+                        axios.get(allPokemonsRaw[i].url)
+                            .then(infoResp => {
+                                res.json(infoResp.data)
+                            })
+                    }
+                })
+                .catch(err => {
+                    res.send(err)
+                });
+        })
+        .catch(err => {
+            res.send(err)
+        });
+});
+
+
 
 module.exports = router;
