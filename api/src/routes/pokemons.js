7fd1c634ebc6ea107374667aefc5fd501502834c
chicha099 <tdsserafin@gmail.com>
2021-09-27 22:33:52 -0300

    front all pokemons en proceso

--- api/src/routes/pokemons.js
+++ api/src/routes/pokemons.js
@@ -9,25 +9,32 @@ router.get("/", (req, res, next) => {
         .then(resp => {
             const next = resp.data.next;
             const firstGet = resp.data.results;
+            //Hasta aca me guardé todos los datos de los primeros 20 pokemons
             axios.get(next)
                 .then(nextResp => {
                     const secondGet = nextResp.data.results;
                     const allPokemonsRaw = firstGet.concat(secondGet);
+                    //Aca ya tengo los otros 20 pokemons y los concatené en un mismo array
                     let listURl = allPokemonsRaw.map(p => p.url);
                     let listPromises = listURl.map(url => axios.get(url));
+                    //Armo la lista/array de URLs a las que voy a pedir la informacion detallada de cada pokemon y creo el array de dichos request
                     Promise.all(listPromises)
                         .then(finalResp => {
                             let allPokesData = finalResp.map(p => p.data);
-                            let finalData = [];
+                            //Aca tengo la informacion detallada de los 40 pokemons
+                            let allPokemonsFinal = [];
                             for (let i = 0; i < allPokesData.length; i++) {
                                 let name = allPokesData[i].name;
-                                //finalResp[i].types
                                 let types = allPokesData[i].types.map(t => t.type.name);
                                 let img = allPokesData[i].sprites.other['official-artwork'].front_default;
-                                finalData.push({ name, types, img })
+                                allPokemonsFinal.push({ name, types, img })
+                                //Declaro variables para el nombre, tipos y foto de cada pokemon y las pusheo a un array vacio
                             }
-                            return res.json(finalData)
+                            return res.json(allPokemonsFinal)
                         })
+                        .catch(err => {
+                            res.send(err)
+                        });
                 })
                 .catch(err => {
                     res.send(err)
--- api/src/routes/types.js
+++ api/src/routes/types.js
@@ -1,13 +1,33 @@
 const { Router } = require("express");
-// const fetch = require("node-fetch");
+const axios = require('axios');
 const { Tipo } = require("../db.js");
+const { all } = require("./pokemons.js");
 
 const router = Router();
 
-// router.get('/', async (req, res) => {
-//     const api = await fetch('https://pokeapi.co/api/v2/type');
-//     const apiJson = api.json();
-//     const types = apiJson.results;
-// })
+router.get("/", (req, res) => {
+    axios.get("https://pokeapi.co/api/v2/type")
+        .then(resp => {
+            let allTypesRaw = resp.data.results;
+            let allTypesFinal = [];
+            for (let i = 0; i < allTypesRaw.length; i++) {
+                let typeName = { name: allTypesRaw[i].name };
+                allTypesFinal.push(typeName)
+            }
+            Tipo.findAll()
+                .then(respCheck => {
+                    let tableCheck = respCheck;
+                    if (!tableCheck.length) {
+                        Tipo.bulkCreate(allTypesFinal)
+                    }
+                })
+                .catch(err => {
+                    res.send(err)
+                });
+        })
+        .catch(err => {
+            res.send(err)
+        });
+})
 
 module.exports = router;
