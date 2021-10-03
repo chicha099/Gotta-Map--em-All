138983f553830adb33b684e93196c0060e2125e6
chicha099 <tdsserafin@gmail.com>
2021-10-03 16:26:04 -0300

    types form ready

--- client/src/components/Create.jsx
+++ client/src/components/Create.jsx
@@ -12,7 +12,7 @@ export default function Detail() {
 
     const [input, setInput] = React.useState({
         Nombre: '',
-        Tipos: [],
+        Tipos: ['normal', 'normal'],
         Imagen: '',
         Vida: 0,
         Fuerza: 0,
@@ -22,6 +22,40 @@ export default function Detail() {
         Altura: 0
     });
 
+    function handleOnChange(e) {
+        e.preventDefault();
+        const name = e.target.name;
+        let value = e.target.value;
+        let id = e.target.id;
+        if (name === 'Tipos' && id === '0') {
+            value = [...input.Tipos, value];
+            console.log(value)
+            value = [e.target.value, value[1]]
+            setInput({
+                ...input,
+                [name]: value
+            })
+        }
+        if (name === 'Tipos' && id === '1') {
+            value = [...input.Tipos, value];
+            console.log(value)
+            value = [value[0], e.target.value]
+            setInput({
+                ...input,
+                [name]: value
+            })
+        }
+        setInput({
+            ...input,
+            [name]: value
+        })
+        console.log(input)
+    };
+
+    // function handleOnChangeSelect(e) {
+    //     e.preventDefault();
+
+    // }
     useEffect(() => {
         dispatch(getTypes());
     }, [dispatch]);
@@ -34,27 +68,27 @@ export default function Detail() {
                 <form className='Form'>
                     <div className='InputsCreate'>
                         <label className=''>Name:</label>
-                        <input type="text" />
+                        <input type="text" name="Nombre" onChange={(e => handleOnChange(e))} value={input.Nombre} />
                     </div>
                     <div className='InputsCreate'>
                         <label>Types:</label>
                         <div className='typeInputs'>
-                            <select className='Options'>
+                            <select id='0' name="Tipos" className='Options' onChange={(e => handleOnChange(e))}>
                                 {
                                     allTypes && allTypes.map(t => {
                                         let typeF = t.name[0].toUpperCase() + t.name.slice(1);
                                         return (
-                                            <option value={t.name}>{typeF}</option>
+                                            <option name="Tipos" value={t.id}>{typeF}</option>
                                         )
                                     })
                                 }
                             </select>
-                            <select className='Options'>
+                            <select id='1' name="Tipos" className='Options' onChange={(e => handleOnChange(e))}>
                                 {
                                     allTypes && allTypes.map(t => {
                                         let typeF = t.name[0].toUpperCase() + t.name.slice(1);
                                         return (
-                                            <option value={t.name}>{typeF}</option>
+                                            <option name="Tipos" value={t.id} >{typeF}</option>
                                         )
                                     })
                                 }
@@ -63,32 +97,35 @@ export default function Detail() {
                     </div>
                     <div className='InputsCreate'>
                         <label>Image:</label>
-                        <input type="url" />
+                        <input type="url" name="Imagen" onChange={(e => handleOnChange(e))} value={input.Imagen} />
 
                     </div>
                     <div className='InputsCreate'>
                         <label>HP:</label>
-                        <input type="number" />
+                        <input type="number" name="Vida" onChange={(e => handleOnChange(e))} value={input.Vida} />
                     </div>
                     <div className='InputsCreate'>
                         <label>Force:</label>
-                        <input type="number" />
+                        <input type="number" name="Fuerza" onChange={(e => handleOnChange(e))} value={input.Fuerza} />
                     </div>
                     <div className='InputsCreate'>
                         <label>Defense:</label>
-                        <input type="number" />
+                        <input type="number" name="Defensa" onChange={(e => handleOnChange(e))} value={input.Defensa} />
                     </div>
                     <div className='InputsCreate'>
                         <label>Speed:</label>
-                        <input type="number" />
+                        <input type="number" name="Velocidad" onChange={(e => handleOnChange(e))} value={input.Velocidad} />
                     </div>
                     <div className='InputsCreate'>
                         <label>Weight:</label>
-                        <input type="number" />
+                        <input type="number" name="Peso" onChange={(e => handleOnChange(e))} value={input.Peso} />
                     </div>
                     <div className='InputsCreate'>
                         <label>Height:</label>
-                        <input type="number" />
+                        <input type="number" name="Altura" onChange={(e => handleOnChange(e))} value={input.Altura} />
+                    </div>
+                    <div >
+                        <button type="submit">Create</button>
                     </div>
                 </form>
             </div>
